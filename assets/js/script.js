93c47dbb710a0ad228147b1ad2bfcf1611b6eb55
rfortega <fjortegadiaz@gmail.com>
2024-10-17 15:46:49 -0300

    tercera parte portafolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 
+//ACTIVACION POPOVER
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 //CAPTURAR FORMULARIO
 
 const formulario_contacto = document.getElementById("form-contacto");
@@ -6,11 +10,12 @@ const formulario_contacto = document.getElementById("form-contacto");
 formulario_contacto.addEventListener("submit", (event) => {
     event.preventDefault(); // Previene la acción por defecto del formulario
 
-        let dataForm = new FormData(formulario_contacto);
+    let dataForm = new FormData(formulario_contacto);
 
-        let nombre = dataForm.get("nombre");
-        let email = dataForm.get("email");
+    let nombre = dataForm.get("nombre");
+    let email = dataForm.get("email");
 
-        alert("Gracias: " + nombre + " por contactarte conmigo, me pondre en contacto a tu email: " + email + "");
+    document.getElementById("mensaje-modal").innerText = ("Gracias " + nombre + " por contactarte conmigo, me pondre en contacto a tu email: " + email + "");
+    document.getElementById("form-contacto").reset();
 
 }); 
